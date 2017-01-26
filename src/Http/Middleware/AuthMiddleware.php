d4540336bacbd48a4d8f2fb4e230d3c63a05ae4d
Luca Tamburo <l.tamburo.shiftleft@gmail.com>
2017-01-26 10:09:55 +0100

    Fix AuthMiddleware

--- src/Http/Middleware/AuthMiddleware.php
+++ src/Http/Middleware/AuthMiddleware.php
@@ -33,7 +33,7 @@ final class AuthMiddleware extends AbstractMiddleware
 
         $accessToken = $this->config->get('access_token');
         if (!($accessToken instanceof AccessToken)) {
-            $accessToken = new AccessToken(json_decode($this->config->get('access_token')));
+            $accessToken = new AccessToken(json_decode($accessToken, true));
         }
 
         $uri = Uri::withQueryValue(
